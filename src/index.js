ecd808863356666e5f699e72262cc15c7ab53304
Dawa Sherpa <dawasherpa1090@gmail.com>
2023-08-05 23:28:01 -0400

    task lister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const main = document.querySelector("#main-content")
+console.log(main)
+
+const taskForm = document.querySelector("#create-task-form")
+const tasklist = document.querySelector("#tasks")
+
+taskForm.addEventListener("submit", function(e) {
+    e.preventDefault()
+   const newTask = document.querySelector("#new-task-description").value
+
+   tasklist.innerHTML += `
+   <li> ${newTask}
+   <button> x for delete </button>
+
+    </li>
+   `
+
+
+
+   
+   //tasklist.appendChild(taskItem)
+   taskForm.reset()
+})
+
+tasklist.addEventListener("click", function(e){
+    console
+})
+
+
+
+
